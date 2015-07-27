12988e529c9e184afc89eafb029260aeaf01374a
Rastopyr <Rastopyr@gmail.com>
2015-07-27 17:53:42 +0300

    Refactor logick

--- src/index.js
+++ src/index.js
@@ -3,76 +3,74 @@ import { Promise } from 'es6-promise';
 import { createClient } from 'redis';
 import { partialRight } from 'ramda';
 
-export function Fabric(opts){
-  let adapter = {};
-  let query = {};
+function QueryFabric(connection) {
+  const queue = [];
+  const query = {};
 
-  let queryQueue = [];
-
-  let addQuery = partialRight(function(query, queue){
-    queue.push(query);
-    return true;
-  },getQueue());
-
-  let clearQueue = function(){
-    (function(queue){ queue.length = 0; })(getQueue());
-    return true;
-  }
-
-  function getQueue(){
-    return queryQueue;
-  }
-
-  let options = opts;
-
-  let execute = function(){
-    return new Promise(function(resolve, reject){
-      let r = createClient(options.port, options.host, {});
-      r.on('error', function(err){reject(err);});
-      r.on('ready', function(){
-        r.select(options.database);
-        let resultSet = [];
-        getQueue().map(function(queryElem){
-          let args = queryElem.slice(1);
-          if(queryElem[0] === 'set'){
-            r.set(args[0], args[1], function(err, reply){
-              resultSet.push(reply.toString());
-            });
-          }
-          else if(queryElem[0] === 'get'){
-            r.get(args[0], function(err, reply){
-              resultSet.push(reply.toString());
-            });
-          }
-          else {
-            reject('Error: wrong query type');
-          }
-        });
-        setTimeout(function(){
-          resolve(resultSet);
-          r.end();
-        }, 50);
-      });
-    });
-  };
-
-  query.set = function(key, value){
+  query.set = function(key, value) {
     addQuery(['set', key, value]);
     return this;
   };
 
-  query.get = function(key){
+  query.get = function(key) {
     addQuery(['get', key]);
     return this;
   };
 
-  query.exec = execute;
-
-  adapter.query = function(){
-    return query;
+  query.exec = function() {
+    queue.reduce(function(sequence, queueItem) {
+      sequence.then(function() {
+        const method = queueItem[0];
+        queueItem.shift();
+        connection[method].apply(connection, queueItem);
+      });
+    }, Promise.resolve(null));
   };
 
-  adapter.exec = execute;
+  query.getQueue = function() {
+
+  };
+
+  query.clearQueue = function() {
+
+  };
+
+  return query;
+}
+
+export function Fabric(options) {
+  let connection = null;
+
+  function getPort(opts) {
+    const optsPort = opts ? opts.port || options.port : options.port;
+
+    return optsPort ? optsPort : 6379;
+  }
+
+  function getHost(opts) {
+    const optsHost = opts ? opts.host || options.host : options.host;
+
+    return optsHost ? optsHost : 'localhost';
+  }
+
+  const adapter = {
+    connect(opts) {
+      connection = createClient(getPort(opts), getHost(opts), {});
+
+      return adapter;
+    },
+    getConnection() {
+      return connection;
+    },
+    select(database) {
+      connection.select(database || options.database);
+
+      return adapter;
+    },
+    query() {
+      return QueryFabric(connection);
+    },
+  };
 
   return adapter;
 }
